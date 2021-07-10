6e4c6ff60983958a83be91b760da5902b9fd1ef2
FaniZeytler <funnyzeytler@gmail.com>
2021-07-10 20:57:28 +0300

    start to build the schedule with grid and text binding

--- exampleApp/exampleApp/exampleApp/Pages/AllSchedule.xaml.cs
+++ exampleApp/exampleApp/exampleApp/Pages/AllSchedule.xaml.cs
@@ -1,25 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
 
 namespace exampleApp.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AllSchedule : ContentPage
     {
-        public IList<Datag>
+        ObservableCollection<Temp> list_bind = new ObservableCollection<Temp>();
+        public ObservableCollection<Temp> List_bind { get { return list_bind; } }
         public AllSchedule()
         {
             InitializeComponent();
-          
-            
+            Peopleview.ItemsSource = list_bind;
+       
 
+
+
+        }
+        private void scheduleButton_Clicked(object sender, EventArgs e)
+        {
+            list_bind = new ObservableCollection<Temp>();
+            Peopleview.ItemsSource = list_bind;
+            List<String> l_temp = new List<string>();
+           for(int i = 0; i <= 10; i++)
+            {
+                l_temp.Add(i.ToString());
+            }
+            list_bind.Add(new Temp { Li = l_temp });
+            list_bind.Add(new Temp { Li = l_temp });
+            frame.IsVisible = true;
+            Peopleview.IsVisible = true;
+        }
+        public class Temp
+        {
+            public List<String> Li { get; set; }
         }
     }
 }
